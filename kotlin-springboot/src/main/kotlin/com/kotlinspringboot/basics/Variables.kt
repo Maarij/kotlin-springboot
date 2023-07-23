8bcd53dde14ad4f85998a613d878fba9a8dab9e2
Maarij <maarij.anwar@gmail.com>
2023-07-23 14:49:58 -0500

    basic types (Int, Long, Double, String)

--- kotlin-springboot/src/main/kotlin/com/kotlinspringboot/basics/Variables.kt
+++ kotlin-springboot/src/main/kotlin/com/kotlinspringboot/basics/Variables.kt
@@ -1,11 +1,26 @@
 fun main() {
-    val name: String = "Maarij"
+    val name = "Maarij"
     println(name);
 
 //    name = "Maarij1"
 
-    var age: Int = 33
-    println(age);
+    var age = 33
+    println(age)
     age = 34
-    println(age);
+    println(age)
+
+    val salary = 10L
+    println(salary)
+
+    var course = "Kotlin Spring"
+    println("course: $course and the course length is ${course.length}")
+
+    val multiLine = "ABC \n DEF"
+    println(multiLine)
+
+    val multiLine1 = """
+        ABC
+        DEF
+    """.trimIndent()
+    println(multiLine1)
 }
