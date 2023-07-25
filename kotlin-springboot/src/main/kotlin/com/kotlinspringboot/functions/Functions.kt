49a0f19c3b1744ba75cf33f11cb1950c336cd849
Maarij <maarij.anwar@gmail.com>
2023-07-24 20:19:47 -0500

    default value parameters & named arguments

--- kotlin-springboot/src/main/kotlin/com/kotlinspringboot/functions/Functions.kt
+++ kotlin-springboot/src/main/kotlin/com/kotlinspringboot/functions/Functions.kt
@@ -1,5 +1,7 @@
 package com.kotlinspringboot.functions
 
+import java.time.LocalDate
+
 fun main() {
     printName("Maarij")
     val result = addition(1, 2)
@@ -7,8 +9,20 @@ fun main() {
 
     val result2 = addition2(1, 2)
     println("Result is $result")
+
+    printPersonDetails("Maarij", "abc@gmail.com", LocalDate.parse("2000-01-01"))
+    printPersonDetails("Maarij")
+    printPersonDetails(name = "Maarij", dob = LocalDate.parse("2000-01-01"))
+    printPersonDetails(dob = LocalDate.parse("2000-01-01"), name = "Maarij", email = "abc@gmail.com")
 }
 
+fun printPersonDetails(
+    name: String,
+    email: String = "",
+    dob: LocalDate = LocalDate.now()
+) {
+    println("Name is $name and email is $email and dob is $dob")
+}
 
 fun addition2(x: Int, y: Int) = x + y
 
