844f221f7bd88d2f65e7adc89458624c4399170e
Maarij <maarij.anwar@gmail.com>
2023-07-29 15:49:58 -0500

    primary constructors

--- kotlin-springboot/src/main/kotlin/com/kotlinspringboot/classes/Person.kt
+++ kotlin-springboot/src/main/kotlin/com/kotlinspringboot/classes/Person.kt
@@ -1,13 +1,20 @@
 package com.kotlinspringboot.classes
 
-class Person {
+class Person(
+    val name: String = "",
+    val age: Int = 0
+) {
 
-    fun action(){
+    fun action() {
         println("Person Walks")
     }
 }
 
 fun main() {
-    val person = Person()
+    val person = Person("Alex", 25)
     person.action()
+    println("Name : ${person.name} and the age is ${person.age}")
+
+    val person1 = Person()
+    println("Name : ${person1.name} and the age is ${person1.age}")
 }
