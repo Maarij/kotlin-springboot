7abbb76caf2905d492a4cae1ce5070a7023377f0
Maarij <maarij.anwar@gmail.com>
2023-07-29 16:58:37 -0500

    companion object keyword (similar to static)

--- kotlin-springboot/src/main/kotlin/com/kotlinspringboot/classes/Inheritance.kt
+++ kotlin-springboot/src/main/kotlin/com/kotlinspringboot/classes/Inheritance.kt
@@ -11,6 +11,12 @@ open class User(val name: String) {
 
 class Student(name: String) : User(name) {
     override var isLoggedIn: Boolean = false
+
+    companion object {
+        const val noOfEnrolledCourses = 10
+        fun country() = "USA"
+    }
+
     override fun login() {
         println("Inside Student Login")
         super.login()
@@ -20,12 +26,15 @@ class Student(name: String) : User(name) {
 class Instructor(name: String) : User(name)
 
 fun main() {
-
     val student = Student("Alex")
     println("name is : ${student.name}")
     student.login()
     student.isLoggedIn = true
     println("Logged in values is : ${student.isLoggedIn}")
+
+    val country = Student.country()
+    println("Country is ${country}")
+    println("noOfEnrolledCourses is ${Student.noOfEnrolledCourses}")
 
     val instructor = Instructor("Maarij")
     println("name is : ${instructor.name}")
