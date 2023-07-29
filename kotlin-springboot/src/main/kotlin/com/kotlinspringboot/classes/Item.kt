06b2c67c8c510db26794ff0583a651c8305d466d
Maarij <maarij.anwar@gmail.com>
2023-07-29 15:55:08 -0500

    secondary constructors

---
+++ kotlin-springboot/src/main/kotlin/com/kotlinspringboot/classes/Item.kt
@@ -0,0 +1,16 @@
+package com.kotlinspringboot.classes
+
+class Item() {
+    var name: String = ""
+
+    constructor(_name: String) : this() {
+        this.name = _name
+    }
+}
+
+fun main() {
+    val item = Item("Iphone")
+    println("Item name is ${item.name}")
+    item.name = "Iphone 13"
+    println("Item name is ${item.name}")
+}
