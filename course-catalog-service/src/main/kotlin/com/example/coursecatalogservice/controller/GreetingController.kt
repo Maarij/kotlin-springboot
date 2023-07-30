702418669206038d7e65351a9c15069e81f82331
Maarij <maarij.anwar@gmail.com>
2023-07-30 09:31:34 -0500

    set up logging

--- course-catalog-service/src/main/kotlin/com/example/coursecatalogservice/controller/GreetingController.kt
+++ course-catalog-service/src/main/kotlin/com/example/coursecatalogservice/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.example.coursecatalogservice.controller
 
 import com.example.coursecatalogservice.service.GreetingsService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/greetings")
 class GreetingController(val greetingsService: GreetingsService) {
 
+    companion object : KLogging()
+
     @GetMapping("/{name}")
     fun retrieveGreetings(@PathVariable name: String): String {
+        logger.info("Name is $name")
         return greetingsService.retrieveGreeting(name)
     }
 }
