f213182aa348413cf4400810db0101eca0204bb8
Maarij <maarij.anwar@gmail.com>
2023-07-30 21:23:06 -0500

    IT for POST endpoint

---
+++ course-catalog-service/src/test/integration/com/example/coursecatalogservice/controller/CourseControllerIT.kt
@@ -0,0 +1,37 @@
+package com.example.coursecatalogservice.controller
+
+import com.example.coursecatalogservice.dto.CourseDto
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIT {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+        val courseDto = CourseDto(null, "Build Restful APIs", "Maarij")
+
+        val savedCourseDto = webTestClient.post()
+            .uri("/v1/courses")
+            .bodyValue(courseDto)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedCourseDto!!.id != null
+        }
+    }
+}
